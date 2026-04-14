2b0db338cddaa144625a6faaff6704de83134346
pankajcoder28 <pankajpenu147@gmail.com>
2026-04-14 14:14:32 +0530

    Refactor project structure and update dependencies
    - Removed unused configuration and service files related to AI models.
    - Simplified the main application logic in app.ts to respond with a basic message.
    - Updated @types/node to version 25.6.0 and undici-types to version 7.19.2.
    - Added new properties for HTTP/2 flow control in undici-types client.
    - Enhanced cache interceptor to allow specific origins for caching.
    - Changed the development script to use 'tsx watch' for better file watching.

--- LangGraph battel-ai-arena/src/app.ts
+++ LangGraph battel-ai-arena/src/app.ts
@@ -1,10 +1,9 @@
 import express from 'express'
-import useGraph from './services/graph.ai.service.js'
 
 const app = express()
 
-app.post('/usermsg',async (req,res)=>{
-   await useGraph("write a factorial function in javascript")
+app.get('/',(req,res)=>{
+   res.send('hello world')
 })
 
 export default app
