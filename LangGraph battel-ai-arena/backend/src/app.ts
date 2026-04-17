00ba9e4bfa54c5a2483609e474922feaa0094e77
pankajcoder28 <pankajpenu147@gmail.com>
2026-04-17 23:54:29 +0530

    feat: add chat interface and arena response components
    - Implemented ArenaResponse component to display two AI solutions with markdown support and judge feedback.
    - Created ChatInterface component for user interaction, including message handling and API integration.
    - Added UserMessage component for displaying user messages in the chat.
    - Configured VSCode settings to ignore unknown CSS at-rules.
    - Added package.json and package-lock.json for dependency management.

--- LangGraph battel-ai-arena/backend/src/app.ts
+++ LangGraph battel-ai-arena/backend/src/app.ts
@@ -1,11 +1,30 @@
 import express from 'express'
 import rungraph from '../src/ai/graph.ai.js'
+import cors from 'cors'
+
 
 const app = express()
+app.use(express.json())
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: ['GET','POST']
+}))
 
 app.get('/',async(req,res)=>{
   const result = await rungraph("write a code for factorial in js ")
   res.json(result)
+})
+
+app.post('/invoke',async (req,res)=>{
+  const {input} = req.body
+  const result = await rungraph(input)
+
+  res.status(200).json({
+    success: true,
+    message: "data fetched sucessfully",
+    result
+  })
 })
 
 export default app
